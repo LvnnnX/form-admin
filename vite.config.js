a20f84dce192486cb3da8c29f00dacb2cc524730
LvnnnX <rickyhadiman@gmail.com>
2026-04-02 17:13:54 +0800

    fixing bugs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // Use absolute base paths for production (important on Vercel)
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false
